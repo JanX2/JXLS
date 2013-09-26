320feb99b5539d11be908c9171f984532a8a347f
Jan Weiß <jan@geheimwerk.de>
2013-09-26 09:26:51 +0200

    Removing casting in DHCell that was made unnecessary due to move to ivars in implementation.

--- DHCell.mm
+++ DHCell.mm
@@ -30,14 +30,14 @@ using namespace xlslib_strings;
 
 @implementation DHCell
 {
-	void	*_cell;
+	xlslib_core::cell_t *_cell;
 }
 
 -(instancetype)initWithCell:(void *)cl
 {
 	self = [super init];
 	
-	_cell = cl;
+	_cell = CELL(cl);
 	
 	return self;
 }
@@ -49,118 +49,118 @@ using namespace xlslib_strings;
 // xf_i interface
 -(void)setFont:(DHFont *)aFont
 {
-	CELL(_cell)->font((font_t *)[aFont font]);
+	_cell->font((font_t *)[aFont font]);
 }
 -(void)setHorizontalAlignment:(halign_option_t)ha_option
 {
-	CELL(_cell)->halign(ha_option);
+	_cell->halign(ha_option);
 }
 -(void)setVerticalAlignment:(valign_option_t)va_option
 {
-	CELL(_cell)->valign(va_option);
+	_cell->valign(va_option);
 }
 -(void)setIndentation:(indent_option_t)indent_option
 {
-	CELL(_cell)->indent(indent_option);
+	_cell->indent(indent_option);
 }
 -(void)setOrientation:(txtori_option_t)ori_option
 {
-	CELL(_cell)->orientation(ori_option);
+	_cell->orientation(ori_option);
 }
 -(void)setForegroundFillColor:(color_name_t)color
 {
-	CELL(_cell)->fillfgcolor(color);
+	_cell->fillfgcolor(color);
 }
 -(void)setBackgroundFillColor:(color_name_t)color
 {
-	CELL(_cell)->fillbgcolor(color);
+	_cell->fillbgcolor(color);
 }
 -(void)setFillStyle:(fill_option_t)fill
 {
-	CELL(_cell)->fillstyle(fill);
+	_cell->fillstyle(fill);
 }
 -(void)setLocked:(BOOL)locked_opt
 {
-	CELL(_cell)->locked((bool)locked_opt);
+	_cell->locked((bool)locked_opt);
 }
 -(void)setHidden:(BOOL)hidden_opt
 {
-	CELL(_cell)->hidden((bool)hidden_opt);
+	_cell->hidden((bool)hidden_opt);
 }
 -(void)setWraps:(BOOL)wrap_opt
 {
-	CELL(_cell)->wrap((bool)wrap_opt);
+	_cell->wrap((bool)wrap_opt);
 }
 -(void)setBorderStyle:(border_style_t)style forSide:(border_side_t)side
 {
-	CELL(_cell)->borderstyle(side, style);
+	_cell->borderstyle(side, style);
 }
 -(void)setBorderColor:(color_name_t)color forSide:(border_side_t)side
 {
-	CELL(_cell)->bordercolor(side, color);
+	_cell->bordercolor(side, color);
 }
 
 //font_i interface
 -(void)setFontName:(NSString *)name
 {
-	CELL(_cell)->fontname([name cStringUsingEncoding:NSASCIIStringEncoding]);
+	_cell->fontname([name cStringUsingEncoding:NSASCIIStringEncoding]);
 }
 -(void)setFontHeight:(uint16_t)fntheight
 {
-	CELL(_cell)->fontheight((unsigned16_t)fntheight);
+	_cell->fontheight((unsigned16_t)fntheight);
 }
 -(void)setFontBold:(boldness_option_t)fntboldness
 {
-	CELL(_cell)->fontbold(fntboldness);
+	_cell->fontbold(fntboldness);
 }
 -(void)setFontUnderline:(underline_option_t)fntunderline
 {
-	CELL(_cell)->fontunderline(fntunderline);
+	_cell->fontunderline(fntunderline);
 }
 -(void)setFontScript:(script_option_t)fntscript
 {
-	CELL(_cell)->fontscript(fntscript);
+	_cell->fontscript(fntscript);
 }
 -(void)setFontColor:(color_name_t)fntcolor
 {
-	CELL(_cell)->fontcolor(fntcolor);
+	_cell->fontcolor(fntcolor);
 }
 -(void)setFontItalic:(BOOL)italic
 {
-	CELL(_cell)->fontitalic((bool)italic);
+	_cell->fontitalic((bool)italic);
 }
 -(void)setFontStrikeout:(BOOL)so
 {
-	CELL(_cell)->fontstrikeout((bool)so);
+	_cell->fontstrikeout((bool)so);
 }
 -(void)setFontOutline:(BOOL)ol
 {
-	CELL(_cell)->fontoutline((bool)ol);
+	_cell->fontoutline((bool)ol);
 }
 -(void)setFontShadow:(BOOL)sh
 {
-	CELL(_cell)->fontshadow((bool)sh);
+	_cell->fontshadow((bool)sh);
 }
 
 // Other
 -(uint32_t)row
 {
-	return CELL(_cell)->GetRow();
+	return _cell->GetRow();
 }
 -(uint32_t)column
 {
-	return CELL(_cell)->GetCol();
+	return _cell->GetCol();
 }
 -(void)setExtendedFormat:(DHExtendedFormat *)extFmt
 {
-	CELL(_cell)->SetXF((xf_t *)[extFmt extendedFormat]);
+	_cell->SetXF((xf_t *)[extFmt extendedFormat]);
 }
 -(DHExtendedFormat *)extendedFormat
 {
 	DHExtendedFormat	*extFmt;
 	xf_t				*fmt;
 	
-	fmt = CELL(_cell)->GetXF();
+	fmt = _cell->GetXF();
 	
 	extFmt = [[DHExtendedFormat alloc] initWithExtendedFormat:fmt];
 	
